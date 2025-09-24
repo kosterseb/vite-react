9a2d317abff192f318bcd9e1385ea31a0ee694ae
kosterseb <sebkos.code@gmail.com>
2025-09-24 10:04:22 +0200

    made a vite-react project for school

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">Welcome to React with Vite!</h1>
+        <p className="text-lg text-gray-600">This is a simple starter template.</p>
+      </div>
+
+      
+      <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+        Click Me
+      </button>
+    </>
+  )
+}
+
+export default App
